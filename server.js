4261fdf9b02caaac66d3a9eedd0aa1f8922cff83
Fel <felgonzalez951@gmail.com>
2024-05-13 16:56:51 -0600

    added fs

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
 const api = require('./routes/index.js');
 
 const PORT = 3001;
@@ -23,6 +24,14 @@ app.get('/notes', (req, res) =>
 app.get('*', (req, res) => 
     res.sendFile(path.join(__dirname, '/public/index.html'))
 );
+
+app.get('/api/notes', (req, res) => 
+    res.sendFile(path.join(__dirname, '/db/db.json'))
+);
+
+// app.post('/api/notes', (req, res) =>
+    
+// )
 
 app.listen(PORT, () => 
     console.log(`App listening at http://localhost:${PORT}`)
