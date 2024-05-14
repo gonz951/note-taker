e5a387ff3171ade1ae2e15f5a50c6c9a55c32e2f
Fel <felgonzalez951@gmail.com>
2024-05-13 18:29:57 -0600

    deleted index file to remove clutter, moved the contents from index to notes.js, added get and post methods, need to finish post method

--- routes/notes.js
+++ routes/notes.js
@@ -1,3 +1,35 @@
-const notes = require('express').Router();
+const router = require('express').Router();
 const { v4: uuidv4 } = require('uuid');
 
+router.get('/notes', (req, res) => 
+    res.sendFile(path.join(__dirname, '../db/db.json'))
+);
+
+router.post('/notes', (req, res) => {
+
+    const { title, text } = req.body;
+
+    if (req.body) {
+        const newNote = {
+            title,
+            text,
+            note_id: uuidv4(),
+        };
+
+        fs.readFile('./db/db.json', 'utf8', (err, data) => {
+            if (err) {
+                console.error(err);
+            } else {
+                const parseNotes = JSON.parse(data);
+                parseNotes.push(newNote);
+                fs.writeFile(
+                    './db/db.json',
+                    JSON.stringify(parseNotes)
+                )
+            }
+        })
+    }
+});
+
+
+module.exports = router;
