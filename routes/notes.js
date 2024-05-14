dba4d09abb18f21e3c2379a98791655eadb2c55f
Fel <felgonzalez951@gmail.com>
2024-05-14 14:46:59 -0600

    added code for deleting and getting notes based on id (doesn't seem to work yet)

--- routes/notes.js
+++ routes/notes.js
@@ -1,36 +1,56 @@
-const router = require('express').Router();
+const notes = require('express').Router();
 const { v4: uuidv4 } = require('uuid');
+const { readFromFile, readAndAppend, writeToFile } = require('../helper/fsUtils');
 
-router.get('/notes', (req, res) => 
-    res.sendFile(path.join(__dirname, '../db/db.json'))
-);
+notes.get('/notes', (req, res) => {
+    // res.sendFile(path.join(__dirname, '../db/db.json'))
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+});
 
-router.post('/notes', (req, res) => {
+// for a specific note
+notes.get('/notes/:note_id', (req, res) => {
+    const noteId = req.params.note_id;
+
+    readFromFile('./db/db.json')
+        .then((data) => JSON.parse(data))
+        .then((json) => {
+            const result = json.filter((note) => note.note_id === noteId);
+            return result.length > 0
+            ? res.json(result)
+            : res.json('No note with that ID');
+        });
+});
+
+notes.delete('/notes/:note_id', (req, res) => {
+    const noteId = req.params.note_id;
+    readFromFile('./db/db.json')
+        .then((data) => JSON.parse(data))
+        .then((json) => {
+            const result = json.filter((note) => note.note_id !== noteId);
+            writeToFile('./db/db.json', result);
+            res.json(`Note ${noteId} has been deleted`);
+        });
+});
+
+notes.post('/notes', (req, res) => {
+    console.log(req.body);
 
     const { title, text } = req.body;
 
-    if (title && text) {
+    if (req.body) {
         const newNote = {
             title,
             text,
             note_id: uuidv4(),
         };
 
-        fs.readFile('./db/db.json', 'utf8', (err, data) => {
-            if (err) {
-                console.error(err);
-            } else {
-                const parseNotes = JSON.parse(data);
-                parseNotes.push(newNote);
-                fs.writeFile(
-                    './db/db.json',
-                    JSON.stringify(parseNotes, null, 4), (err) =>
-                    err ? console.error(err) : console.info(`\nData written to db.json`)
-                 )
-            }
-        })
+        //console.log(req.params.note_id)
+        readAndAppend(newNote, './db/db.json');
+        res.json('Note added.');
+    } else {
+        res.error('Error in adding note.');
     }
 });
 
 
-module.exports = router;
+module.exports = notes;
